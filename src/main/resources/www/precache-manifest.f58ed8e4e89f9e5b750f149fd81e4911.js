e8fcb99118eddc2ae83c00ec1e982cfd3b63d150
malyshgit <vm.malysh@gmail.com>
2019-11-23 13:26:55 +0500

    fix

---
+++ src/main/resources/www/precache-manifest.f58ed8e4e89f9e5b750f149fd81e4911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d11206147385b55a945b3bb08ff32c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0fb582bdaebcab02ea",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b76bb0dc4a15e28a83b9",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4b0fb582bdaebcab02ea",
+    "url": "/static/js/2.a84b0a85.chunk.js"
+  },
+  {
+    "revision": "b76bb0dc4a15e28a83b9",
+    "url": "/static/js/main.8ac8211d.chunk.js"
+  },
+  {
+    "revision": "5e5335aeaf8d315fd672",
+    "url": "/static/js/runtime-main.11c5cfba.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
